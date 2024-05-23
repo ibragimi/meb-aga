60fc4d032238541856d047b753adaaac8442b8b6
ibragimi <sintiklot@gmail.com>
2024-05-23 23:03:48 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+    $(".fancybox").fancybox({
+        openEffect	: 'none',
+        closeEffect	: 'none'
+    });
+});
+
+
+
+
+$(function() {    var url=document.location.href;
+    $('.menu a').each(function()
+    { if ( this.href==url )
+    {
+        $(this).addClass('act');
+    };
+    });
+});
+
+
+
+
+
+
+
+$("#efect").fadeIn(6000);
+$("#efect2").fadeIn(15000);
+$("#efect3").fadeIn(8000);
+
+
+
+
+
+(function(){ var widget_id = '1332161679';
+ var s = document.createElement('script');
+ s.type = 'text/javascript'; s.async = true;
+ s.src = 'https://сайтофициально.рф/script/js/callback/'+widget_id+'/callback_st_of.js';
+ var ss = document.getElementsByTagName('script')[0];
+ ss.parentNode.insertBefore(s, ss);})();
+
+
